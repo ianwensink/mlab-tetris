feb5c91316c2902cf6371568f6b42a10c799ef4b
Ian Wensink <ian@burst-digital.com>
2017-06-19 17:02:44 +0200

    Change colours

--- src/client/Piece/PieceFactory.ts
+++ src/client/Piece/PieceFactory.ts
@@ -5,10 +5,10 @@ export default class PieceFactory {
   public static getPiece(game: Game, player: number): Piece {
     switch(player) {
       case 2:
-        return new Piece(2, game, '#FF0', 'rgba(122, 122, 0, 0.3)', { left: 65, rotate: 87, right: 68 });
+        return new Piece(2, game, '#32cc1a', 'rgba(50, 204, 26, 0.3)', { left: 65, rotate: 87, right: 68 });
       case 1:
       default:
-        return new Piece(1, game, '#F00', 'rgba(87, 0, 0, 0.3)', { left: 37, rotate: 38, right: 39 });
+        return new Piece(1, game, '#ffeb21', 'rgba(255, 235, 33, 0.3)', { left: 37, rotate: 38, right: 39 });
     }
   }
 }
