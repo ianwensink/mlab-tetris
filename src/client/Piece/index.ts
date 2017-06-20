105d49b5f4325d625da4c586ce4b6bc9fbb6ed06
Ian Wensink <ian@burst-digital.com>
2017-06-20 23:28:12 +0200

    Improve performance

--- src/client/Piece/index.ts
+++ src/client/Piece/index.ts
@@ -18,7 +18,8 @@ export default class Piece {
   private shadowColor: string;
   private controls: IControls;
   private autoMoveDownInterval: number;
-  private animationUpdateInterval: number;
+  private animationUpdateTimeout: number;
+  private animationUpdateLastCall: number;
   private animPositionX: number = 0;
   private animPositionY: number = 0;
   private animRotation: number = 0;
@@ -157,7 +158,7 @@ export default class Piece {
     this.scc = this.sc.getContext('2d') as CanvasRenderingContext2D;
 
     this.autoMoveDownInterval = window.setInterval(() => this.moveDownIntervalFunc(), this.fallInterval);
-    this.animationUpdateInterval = window.setInterval(() => this.animationUpdateIntervalFunc(), 16);
+    this.animationUpdateIntervalFunc();
 
     this.updateSizing();
 
@@ -166,9 +167,13 @@ export default class Piece {
 
   public update() {
     if(this.game.state === Game.states.STARTED) {
-      this.drawPiece(this.acc);
-      this.drawShadow(this.scc);
+      this.draw();
     }
+  }
+
+  public draw() {
+    this.drawPiece(this.acc);
+    this.drawShadow(this.scc);
   }
 
   public onClickedKey(data: string) {
@@ -190,7 +195,6 @@ export default class Piece {
 
   public unMount() {
     clearInterval(this.autoMoveDownInterval);
-    clearInterval(this.animationUpdateInterval);
 
     this.acc.clearRect(0, 0, this.game.boardOffsetX * 2 + this.game.tileSizeX * this.game.boardSizeX + this.game.tileGapSize * (this.game.boardSizeX - 1), this.game.boardOffsetY * 2 + this.game.tileSizeY * this.game.boardSizeY + this.game.tileGapSize * (this.game.boardSizeY - 1));
     this.acc.save();
@@ -206,13 +210,19 @@ export default class Piece {
     this.update();
   }
 
-  private animationUpdateIntervalFunc() {
-    // move animPositions closer to their targets (piece positions)
-    this.animPositionX += (this.pieceX - this.animPositionX) * 0.3;
-    this.animPositionY += (this.pieceY - this.animPositionY) * 0.3;
-    // move animRotation closer to zero
-    this.animRotation -= this.animRotation * 0.3;
-    this.update();
+  private animationUpdateIntervalFunc(now: number = 16) {
+    this.animationUpdateTimeout = window.setTimeout(() => {
+      window.requestAnimationFrame((time: number) => {
+        // move animPositions closer to their targets (piece positions)
+        this.animPositionX += (this.pieceX - this.animPositionX) * 0.3;
+        this.animPositionY += (this.pieceY - this.animPositionY) * 0.3;
+        // move animRotation closer to zero
+        this.animRotation -= this.animRotation * 0.3;
+        this.update();
+        this.animationUpdateIntervalFunc(time);
+        this.animationUpdateLastCall = time;
+      });
+    }, 32);
   }
 
   private nextPiece() {
@@ -334,6 +344,7 @@ export default class Piece {
   }
 
   private drawPiece(context: CanvasRenderingContext2D) {
+    // console.trace('draw');
     // drawing using geometry of current rotation
     const tetk = this._tetrominos[ this.curPiece ][ this.curRotation ];
     // translating (canvas origin) to the center,
